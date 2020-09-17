9d1c659a33578867acf2e4712a0bd42df92d6061
aisajay <aisajay@XPAP1-D-2533>
2020-09-17 21:27:53 +0530

    Application code complete.

---
+++ PromotionEngine/CartModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class CartModel
+    {
+        
+            public string Unit { get; set; }
+            public int Price { get; set; }
+
+
+            public CartModel(string id)
+            {
+                this.Unit = id;
+                switch (id)
+                {
+                    case "A":
+                        this.Price = 50;
+
+                        break;
+                    case "B":
+                        this.Price = 30;
+
+                        break;
+                    case "C":
+                        this.Price = 20;
+
+                        break;
+                    case "D":
+                        this.Price = 15;
+                        break;
+                }
+            }
+
+        
+    }
+}
--- PromotionEngine/Program.cs
+++ PromotionEngine/Program.cs
@@ -8,9 +8,62 @@ namespace PromotionEngine
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+
+            Console.WriteLine("Enter the Number of Items in the Cart");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            List<CartModel> items = new List<CartModel>();
+                       
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("enter any one of these items to add to cart A or B or C or D");
+                string type = Console.ReadLine();
+                CartModel p = new CartModel(type);
+                items.Add(p);
+            }
+
+            int totalPrice = GetCheckoutPrice(items);
+            Console.WriteLine(totalPrice);
+            Console.ReadLine();
+        }
+
+        private static int GetCheckoutPrice(List<CartModel> items)
+        {
+            int counterofA = 0;
+            int priceofA = 50;
+            int counterofB = 0;
+            int priceofB = 30;
+            int CounterofC = 0;
+            int priceofC = 20;
+            int CounterofD = 0;
+            int priceofD = 15;
+            foreach (CartModel pr in items)
+            {
+                if (pr.Unit == "A" || pr.Unit == "a")
+                {
+                    counterofA = counterofA + 1;
+                }
+                if (pr.Unit == "B" || pr.Unit == "b")
+                {
+                    counterofB = counterofB + 1;
+                }
+                if (pr.Unit == "C" || pr.Unit == "c")
+                {
+                    CounterofC = CounterofC + 1;
+                }
+                if (pr.Unit == "D" || pr.Unit == "d")
+                {
+                    CounterofD = CounterofD + 1;
+                }
+            }
+            int totalPriceofA = (counterofA / 3) * 130 + (counterofA % 3 * priceofA);
+            int totalPriceofB = (counterofB / 2) * 45 + (counterofB % 2 * priceofB);
+            int totalPriceofC = (CounterofC * priceofC);
+            int totalPriceofD = (CounterofD * priceofD);
+            return totalPriceofA + totalPriceofB + totalPriceofC + totalPriceofD;
+
         }
     }
 }
